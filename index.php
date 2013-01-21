f7f41f83ffcb9e9bcdf6dd8e5acc4a08db9b3b59
Yurienu <yurienu@myopera.com>
2013-01-21 16:20:33 +0100

    Force désactivation magic quotes

--- index.php
+++ index.php
@@ -20,6 +20,23 @@
 ### Initialisation
 define('ROOT', dirname( __FILE__ ));
 
+# Force la désactivation des guillemets magiques
+if(get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
 # En-tête
 header('Content-Type: text/html; charset=utf-8');
 
