657a894462f242fa4a19a3251d19c204f5a99371
Yurienu <yurienu@myopera.com>
2013-02-21 23:43:03 +0100

    Bootstrap + RainTPL

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -45,14 +45,14 @@ if(get_magic_quotes_gpc())
 header('Content-Type: text/html; charset=utf-8');
 
 ## Récupération de la configuration
-$file = array('conf'       => file_get_contents(ROOT.'/config/constants.json'),
-              'ressources' => file_get_contents(ROOT.'/config/ressources.json'),
-              'dimensions' => file_get_contents(ROOT.'/config/dimensions.json'));
+$file = array('conf'       => file_get_contents(ROOT.'/data/constants.json'),
+              'ressources' => file_get_contents(ROOT.'/data/ressources.json'),
+              'dimensions' => file_get_contents(ROOT.'/data/dimensions.json'));
 
 $conf       = json_decode($file['conf'], true);
 $ressources = json_decode($file['ressources'], true);
 $dimensions = json_decode($file['dimensions'], true);
-$file_identifier = is_readable($conf['URL_IDENTIFIER']) ? $conf['URL_IDENTIFIER'] : ROOT.'/identifier';
+$file_identifier = is_readable($conf['URL_IDENTIFIER']) ? $conf['URL_IDENTIFIER'] : ROOT.'/data/identifier';
 $file['identifier'] = file($file_identifier, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 # Récupération du cookie
@@ -80,7 +80,8 @@ $already_selected = false;
 $timestamp = $conf['FIRST_WEEK'];
 for($i = 0; $i < $conf['NB_WEEKS']; ++$i)
 {
-  $weeks[$i] = $timestamp;
+  $weeks[$i] = array('timestamp' => $timestamp,
+                     'date'      => gmdate('d\/m\/Y', $timestamp));
 
   # Semaine suivante (seulement 6 jours pour l’instant pour capter la semaine courante au dimanche)
   $timestamp += 6 * 24 * 3600;
@@ -126,134 +127,49 @@ else
 # Le format (horizontal/vertical)
 $displayConfId = isset($perconf) ? intval($perconf['displayConfId']) : $conf['DISPLAY_CONF_ID'];
 
+# On prépare l’affichage des ressources
+$ressources_display = array();
+foreach($ressources as $kLoop => $vLoop)
+  $ressources_display[$kLoop] = array('selected' => in_array($vLoop, $idTree),
+                                      'value'    => $vLoop);
+
 # On prépare l’export en iCal
 list($startDay, $startMonth, $startYear) = explode('/', gmdate('d\/m\/Y', $conf['FIRST_WEEK']));
 list($endDay, $endMonth, $endYear) = explode('/', gmdate('d\/m\/Y', intval($conf['FIRST_WEEK'] + ($conf['NB_WEEKS'] * 7 * 24 * 3600))));
 
 # On prépare l’URL de l’image
-$img_src = (implode(',', $idTree) != 0) ? $conf['URL_ADE'].'/imageEt?identifier='.$identifier.'&amp;projectId='.$conf['PROJECT_ID'].'&amp;idPianoWeek='.$idPianoWeek.'&amp;idPianoDay='.$idPianoDay.'&amp;idTree='.implode(',', $idTree).'&amp;width='.$width.'&amp;height='.$height.'&amp;lunchName=REPAS&amp;displayMode=1057855&amp;showLoad=false&amp;ttl='.time().'000&amp;displayConfId='.$displayConfId : 'img/bgExpertBlanc.gif';
-?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
-<head>
-  <meta charset="utf-8" />
+$img_src = (implode(',', $idTree) != 0) ? $conf['URL_ADE'].'/imageEt?identifier='.$identifier.'&amp;projectId='.$conf['PROJECT_ID'].'&amp;idPianoWeek='.$idPianoWeek.'&amp;idPianoDay='.$idPianoDay.'&amp;idTree='.implode(',', $idTree).'&amp;width='.$width.'&amp;height='.$height.'&amp;lunchName=REPAS&amp;displayMode=1057855&amp;showLoad=false&amp;ttl='.time().'000&amp;displayConfId='.$displayConfId : 'static/img/bgExpertBlanc.gif';
 
-  <title>Planning IUT Info</title>
-  <meta name="description" content="Planning IUT Info est une application en PHP pour récupérer le planning de l’ADE sur l’ENT de l’IUT Informatique d’Aix-en-Provence via une interface suivant le principe KISS" />
-  <meta name="keywords" content="planning, emploi, temps, ade, ent, iut, dut, info, informatique, aix, aix-en-provence" />
-  <meta name="author" content="Julien Papasian" />
-  <meta name="robots" content="noindex, nofollow" />
+### Préparation du template
+# Template
+require_once(ROOT.'/lib/rain.tpl.class.php');
+$tpl = new Rain\Tpl;
+$tpl->configure('tpl_dir',   'tpl/');
+$tpl->configure('cache_dir', 'tmp/');
+$tpl->assign(array('img_src'         => $img_src,
+                   'json_conf'       => json_encode($conf),
+                   'json_dimensions' => json_encode($dimensions),
+                   'identifier'      => $identifier,
+                   'idTree_implode'  => implode(',',$idTree),
+                   'startDay'        => $startDay,
+                   'startMonth'      => $startMonth,
+                   'startYear'       => $startYear,
+                   'endDay'          => $endDay,
+                   'endMonth'        => $endMonth,
+                   'endYear'         => $endYear,
+                   'conf'            => $conf,
+                   'ressources'      => $ressources_display,
+                   'idPianoWeek'     => $idPianoWeek,
+                   'saturday'        => $saturday,
+                   'sunday'          => $sunday,
+                   'dimensions'      => $dimensions,
+                   'width'           => $width,
+                   'weeks'           => $weeks,
+                   'displayConfId'   => $displayConfId,
+                   'this_year'       => date('Y')));
 
-  <?= (implode(',', $idTree) != 0) ? '<link rel="alternate" type="application/rss+xml" title="Flux RSS des '.$conf['NB_DAYS_RSS'].' jours à venir" href="'.$conf['URL_ADE'].'/rss?projectId='.$conf['PROJECT_ID'].'&amp;resources='.implode(',', $idTree).'&amp;nbDays='.$conf['NB_DAYS_RSS'].'" />' : '' ?>
-  <link rel="stylesheet" title="Planning" type="text/css" href="static/style.css" />
+$tpl->draw('layout.head');
+$tpl->draw('planning');
+$tpl->draw('layout.foot');
 
-  <script type="text/javascript">
-  // <![CDATA[
-  /* Quelques trucs en CSS pour ceux qui ont JavaScript désactivé */
-  document.write('<style type="text/css">');
-  document.write('  input[type="submit"] { display: none; }');
-  document.write('  button.week { display: inline; }');
-  document.write('  #url { display: none; }');
-  <?= (implode(',',$idTree) != 0) ? 'document.write(\'  #genbutton { display: inline; }\');' : '' ?>
-  document.write('</style>');
-  // ]]>
-  </script>
-</head>
-<body>
-  <header><h1>Planning IUT Info</h1></header>
-
-  <hr />
-
-  <form id="planning" method="post" action="index.php">
-    <table class="selectors">
-      <tbody>
-        <tr>
-          <td colspan="3">
-            <select name="idTree[]" id="idTree" multiple="multiple">
-              <?php
-              $first_optgroup = true;
-              foreach($ressources as $kLoop => $vLoop)
-              {
-                if($vLoop != 0)
-                  echo '<option value="'.$vLoop.'"'.((in_array($vLoop, $idTree)) ? ' selected="selected"' : '').'>'.$kLoop.'</option>';
-
-                else
-                {
-                  echo (!$first_optgroup ? '</optgroup>' : '').'<optgroup label="'.$kLoop.'">';
-                  $first_optgroup = false;
-                }
-              }
-              echo '</optgroup>';
-              ?>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <?= ($idPianoWeek > 0) ? '<button id="previous_week" class="week">&lt;&lt;</button>' : '&nbsp;' ?>
-          </td>
-
-          <td>
-            <select name="idPianoWeek" id="idPianoWeek">
-              <?php
-              # Boucle sur NB_WEEKS semaines
-              for($i = 0; $i < $conf['NB_WEEKS']; ++$i)
-              {
-                echo '<option value="'.$i.'"'.(($idPianoWeek == $i) ? ' selected="selected"' : '').'>Semaine du '.gmdate('d\/m\/Y', $weeks[$i]).'</option>';
-                $timestamp += 7 * 24 * 3600;
-              }
-              ?>
-            </select>
-          </td>
-
-          <td>
-            <?= ($idPianoWeek < $conf['NB_WEEKS'] - 1) ? '<button id="next_week" class="week">&gt;&gt;</button>' : '&nbsp;' ?>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <hr />
-    <p><img id="img_planning" src="<?= $img_src ?>" alt="Serveur inaccessible ou mise à jour requise" /></p>
-    <hr />
-
-    <p>
-      <input type="checkbox" name="saturday" id="saturday" value="yes"<?= ($saturday == 'yes') ? ' checked="checked"' : '' ?> /><label for="saturday"> Samedi</label>
-      <input type="checkbox" name="sunday" id="sunday" value="yes"<?= ($sunday == 'yes') ? ' checked="checked"' : '' ?> /><label for="sunday"> Dimanche</label>
-    </p>
-
-    <p>
-      <select id="displayConfId" name="displayConfId">
-        <option value="41"<?= ($displayConfId == 41) ? ' selected="selected"' : '' ?>>Horizontal</option>
-        <option value="8"<?= ($displayConfId == 8) ? ' selected="selected"' : '' ?>>Vertical</option>
-      </select>
-      <select id="width" name="width">
-        <?php
-        foreach($dimensions as $dWidth => $dHeight)
-          echo '<option value="'.$dWidth.'"'.(($width == $dWidth) ? ' selected="selected"' : '').'>'.$dWidth.' x '.$dHeight.''.(($dWidth == $conf['WIDTH'] && $dHeight == $conf['HEIGHT']) ? ' (par défaut)' : '').'</option>';
-        ?>
-      </select>
-    </p>
-
-    <p><input type="submit" name="submit" value="Récupérer le planning" /><button id="genbutton">Exporter en iCal</button></p>
-
-    <?php
-    echo '<fieldset id="url"><legend>URL d’export du calendrier au format iCal</legend><p>'.$conf['URL_ADE'].'<wbr />/custom<wbr />/modules<wbr />/plannings<wbr />/anonymous_cal.jsp?<wbr />resources=<span id="resources">'.implode(',',$idTree).'</span><wbr />&amp;projectId='.$conf['PROJECT_ID'].'<wbr />&amp;startDay='.$startDay.'<wbr />&amp;startMonth='.$startMonth.'<wbr />&amp;startYear='.$startYear.'<wbr />&amp;endDay='.$endDay.'<wbr />&amp;endMonth='.$endMonth.'<wbr />&amp;endYear='.$endYear.'<wbr />&amp;calType=ical</p></fieldset>';
-    ?>
-  </form>
-
-  <hr />
-
-  <footer><p>Copyright © 2012-<?= date('Y') ?> <a href="https://github.com/Yurienu/PlanningIUTInfo">Planning IUT Info</a></p></footer>
-
-  <script type="text/javascript">
-  // <![CDATA[
-  var conf       = <?= json_encode($conf) ?>;
-  var dimensions = <?= json_encode($dimensions) ?>;
-  var identifier = '<?= $identifier ?>';
-  // ]]>
-  </script>
-  <script type="text/javascript" src="static/form.js"></script>
-  <script type="text/javascript" src="static/listeners.js"></script>
-</body>
-</html>
+/** EOF /**/
