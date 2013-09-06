34a94ae1af0e66d846fbfc792dec7cf506168aa3
Yurienu <yurienu@myopera.com>
2013-09-06 18:51:38 +0200

    Ajout d’un script pour réinitialiser l’identifiant en cURL

---
+++ reset.php
@@ -0,0 +1,51 @@
+<?php
+# En-tête
+header('Content-Type: text/html; charset=utf-8');
+
+# Initialisation de la session cURL
+$ch = curl_init();
+
+# Se connecte au portail Univ-AMU
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/custom/modules/plannings/anonymous_cal.jsp?resources=1467&projectId=26&startDay=26&startMonth=08&startYear=2013&endDay=25&endMonth=08&endYear=2014&calType=ical');
+curl_setopt($ch, CURLOPT_HEADER, true);         # Affiche les headers (pour récupérer le cookie)
+curl_setopt($ch, CURLOPT_NOBODY, true);         # Affiche UNIQUEMENT les headers (pas le contenu)
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); # Affiche le contenu sous forme de string
+
+# Récupère le cookie
+$content = curl_exec($ch);
+preg_match_all('|Set-Cookie: (.*);|U', $content, $results);
+$cookies = implode(';', $results[1]);
+
+# Envoie le cookie
+curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+curl_setopt($ch, CURLOPT_HEADER, false); # Désactive l’affichage des headers
+
+## Sélectionne une ressource
+# Déroule le menu des enseignants
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/standard/gui/tree.jsp?category=instructor&expand=false&forceLoad=false&reload=false&scroll=0');
+curl_exec($ch);
+
+# Déroule la lettre N
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/standard/gui/tree.jsp?branchId=6270&expand=false&forceLoad=false&reload=false&scroll=0');
+curl_exec($ch);
+
+# Sélectionne Nedjar
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/standard/gui/tree.jsp?selectId=1467&reset=false&forceLoad=true&scroll=0');
+curl_exec($ch);
+
+# Sélectionne les jours
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/custom/modules/plannings/pianoDays.jsp');
+curl_exec($ch);
+
+# Sélectionne les semaines et récupère l’image
+curl_setopt($ch, CURLOPT_NOBODY, false); # Réactive la récupération du contenu de la page
+curl_setopt($ch, CURLOPT_URL, 'http://planning.univ-amu.fr/ade/custom/modules/plannings/imagemap.jsp?week=6&reset=false&width=1360&height=591');
+$image = curl_exec($ch);
+
+# Récupération de l’identifiant
+preg_match('|identifier=(.*)&|U', $image, $identifier);
+file_put_contents('data/identifier', $identifier[1]);
+echo 'Nouvel identifiant réinitialisé&nbsp;: '.$identifier[1];
+
+curl_close($ch);
+/** EOF /**/
