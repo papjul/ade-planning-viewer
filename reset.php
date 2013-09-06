33a46fd4a9e61b9bbe496e865ca993a269d4cf45
Yurienu <yurienu@myopera.com>
2013-09-06 19:03:28 +0200

    Protection du reset.php : le script ne peut être appelé que toutes les heures

--- reset.php
+++ reset.php
@@ -20,6 +20,9 @@
 # En-tête
 header('Content-Type: text/html; charset=utf-8');
 
+# Ce script ne peut être appelé que toutes les heures maximum pour des raisons de sécurité
+if(filemtime('data/identifier') > time() - 3600) exit('L’identifiant de connexion a déjà été réinitialisé il y a peu de temps.');
+
 # Initialisation de la session cURL
 $ch = curl_init();
 
