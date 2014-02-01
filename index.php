fe2f75d3c257a762fb89ab427812da69bf5203bb
papjul <julien.papasian@gmail.com>
2014-02-01 20:23:21 +0100

    Update header licenses

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-/**
+/** 
  * Planning IUT Info
  * Copyright © 2012-2014 Julien Papasian
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
+ * modify it under the terms of the Affero General Public License
+ * as published by Affero; either version 1 of the License, or (at
+ * your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Affero General Public License for more details.
+ * 
+ * You should have received a copy of the Affero General Public
+ * License along with this program. If not, see
+ * <http://www.affero.org/oagpl.html>.
  */
 
 ### Initialisation
